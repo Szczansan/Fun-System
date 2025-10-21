6a2b799cee5ee554719aece02798e9e91a8c1b5b
Szczansan <zansanbadri@gmail.com>
2025-10-21 22:16:10 +0700

    Add files via upload

---
+++ System.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+from streamlit_option_menu import option_menu
+import Logistic as Logistic
+
+st.markdown(
+    """
+    <h1 style='text-align: center; color: #2E86C1;'>
+        Welcome
+    </h1>
+    """,
+    unsafe_allow_html=True
+)
+# Feature
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.image("warehouse.png", width=100)
+    if st.button("Logistic"):
+        st.session_state.page = "Logistic"
+with col2:
+    if st.button("PPC"):
+        st.session_state.page = "PPC"
+with col3:
+    if st.button("Produksi"):
+        st.session_state.page = "Produksi"
+
+if "page" not in st.session_state:
+    st.session_state.page = "home"
+if st.session_state.page == "Logistic":
+    Logistic.show_page()
+
+
+
+
+
+
+
+
+
+
+
+
